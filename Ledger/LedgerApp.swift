e0508b4ab6658db7b7b6ebced66b4fe661169df5
Rick <liuqiang1026@hotmail.com>
2024-11-29 23:39:20 +0800

    Initial Commit

---
+++ Ledger/LedgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LedgerApp.swift
+//  Ledger
+//
+//  Created by Rick on 2024/11/29.
+//
+
+import SwiftUI
+
+@main
+struct LedgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
